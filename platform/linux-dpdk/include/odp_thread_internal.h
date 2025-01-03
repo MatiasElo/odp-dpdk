618e5dd2f55a118338f5759850bc7e87378c6d20
Matias Elo <matias.elo@nokia.com>
2025-01-03 13:45:01 +0200

    Port 477ac44ad "linux-gen: cpu: remove static cpu id config file option"
    Port original commit from linux-generic.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>

--- platform/linux-dpdk/include/odp_thread_internal.h
+++ platform/linux-dpdk/include/odp_thread_internal.h
@@ -12,14 +12,14 @@ extern "C" {
 #include <stdint.h>
 
 /**
- * Read CPU IDs of active ODP threads
+ * Read IDs of active ODP threads
  *
- * @param[out] cpu_ids      CPU ID array
- * @param      max_num      Maximum number of CPU IDs to write
+ * @param[out] ids          Thread ID array
+ * @param      max_num      Maximum number of thread IDs to write
  *
- * @return Number of CPU IDs written to the output array
+ * @return Number of thread IDs written to the output array
  */
-int _odp_thread_cpu_ids(unsigned int cpu_ids[], int max_num);
+int _odp_thread_ids(unsigned int ids[], int max_num);
 
 /**
  * Read current epoch value of thread mask all
